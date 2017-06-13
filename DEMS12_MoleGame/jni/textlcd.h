f0d7903d164a12cdc35eb762489690ee2e84d4ea
jeonchanghyeon <jchyeon0814@naver.com>
2017-06-13 15:15:42 +0900

    new

---
+++ DEMS12_MoleGame/jni/textlcd.h
@@ -0,0 +1,12 @@
+#ifndef TEXTLCD_H_
+#define TEXTLCD_H_
+
+#define TEXTLCD_ON 1
+#define TEXTLCD_OFF 2
+#define TEXTLCD_INIT 3
+#define TEXTLCD_CLEAR 4
+
+#define TEXTLCD_LINE1 5
+#define TEXTLCD_LINE2 6
+
+#endif
